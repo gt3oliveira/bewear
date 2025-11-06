8ecfdf67f0ebcf698161750ad2deafa3a2c01b35
gt3oliveira <gt3oliveira@gmail.com>
2025-11-06 18:16:15 -0300

    feat: add constant simple-icons

---
+++ src/constants/simple-icons.ts
@@ -0,0 +1,18 @@
+export const simpleIcons = [
+    {
+        label: 'Nike',
+        icon: './simple-icons_nike.svg'
+    },
+    {
+        label: 'Adidas',
+        icon: './simple-icons_adidas.svg'
+    },
+    {
+        label: 'Puma',
+        icon: './simple-icons_puma.svg'
+    },
+    {
+        label: 'New Balance',
+        icon: './simple-icons_newbalance.svg'
+    }
+]
