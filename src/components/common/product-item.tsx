25a11c0c92f75f81b0f3492886539d1655d463e8
gt3oliveira <gt3oliveira@gmail.com>
2025-11-06 19:21:12 -0300

    chore: add merge de classname no componente product-item

--- src/components/common/product-item.tsx
+++ src/components/common/product-item.tsx
@@ -3,14 +3,16 @@ import Link from 'next/link'
 
 import { productTable, productVariantTable } from '@/db/schema'
 import { formatCentsToBRL } from '@/helpers/money'
+import { cn } from '@/lib/utils'
 
 interface ProductItemProps {
   product: typeof productTable.$inferSelect & {
     variants: (typeof productVariantTable.$inferSelect)[]
   }
+  className?: string
 }
 
-export const ProductItem = ({ product }: ProductItemProps) => {
+export const ProductItem = ({ product, className }: ProductItemProps) => {
   const firstVariant = product.variants[0]
 
   return (
@@ -18,12 +20,13 @@ export const ProductItem = ({ product }: ProductItemProps) => {
       <Image
         src={firstVariant.imageUrl}
         alt={firstVariant.name}
-        width={200}
-        height={200}
-        className="rounded-3xl"
+        width={0}
+        height={0}
+        sizes="100vw"
+        className="h-auto w-full rounded-3xl"
       />
 
-      <div className={`flex max-w-[200px] flex-col gap-1`}>
+      <div className={cn('flex max-w-[200px] flex-col gap-1', className)}>
         <p className="truncate text-sm font-medium">{product.name}</p>
         <p className="text-muted-foreground truncate text-xs font-medium">
           {product.description}
