3fd3273f9332e94dd652e16641e873f06e132e4c
gt3oliveira <gt3oliveira@gmail.com>
2025-11-06 15:05:50 -0300

    feat: add product-list and product-item to pages, and moved header of pages to layout

--- src/app/authentication/page.tsx
+++ src/app/authentication/page.tsx
@@ -1,32 +1,23 @@
-import { Header } from "@/components/common/header"
-import {
-  Tabs,
-  TabsContent,
-  TabsList,
-  TabsTrigger,
-} from "@/components/ui/tabs"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 
-import { SignInForm } from "./_components/sign-in-form"
-import { SignUpForm } from "./_components/sign-up-form"
+import { SignInForm } from './_components/sign-in-form'
+import { SignUpForm } from './_components/sign-up-form'
 
 export default function AuthenticationPage() {
   return (
-    <>
-      <Header />
-      <div className="flex w-full flex-col gap-6 p-5">
-        <Tabs defaultValue="sign-in">
-          <TabsList>
-            <TabsTrigger value="sign-in">Entrar</TabsTrigger>
-            <TabsTrigger value="sign-up">Criar Conta</TabsTrigger>
-          </TabsList>
-          <TabsContent value="sign-in">
-            <SignInForm />
-          </TabsContent>
-          <TabsContent value="sign-up">
-            <SignUpForm />
-          </TabsContent>
-        </Tabs>
-      </div>
-    </>
+    <div className="flex w-full flex-col gap-6 p-5">
+      <Tabs defaultValue="sign-in">
+        <TabsList>
+          <TabsTrigger value="sign-in">Entrar</TabsTrigger>
+          <TabsTrigger value="sign-up">Criar Conta</TabsTrigger>
+        </TabsList>
+        <TabsContent value="sign-in">
+          <SignInForm />
+        </TabsContent>
+        <TabsContent value="sign-up">
+          <SignUpForm />
+        </TabsContent>
+      </Tabs>
+    </div>
   )
-};
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,36 @@
-import { Header } from "@/components/common/header";
+import Image from 'next/image'
 
-export default function Home() {
+import { ProductList } from '@/components/common/product-list'
+import { db } from '@/db'
+
+export default async function Home() {
+  const products = await db.query.productTable.findMany({
+    with: {
+      variants: true,
+    },
+  })
+
   return (
-    <div>
-      <Header />
+    <div className="space-y-6">
+      <Image
+        src="/banner-01.svg"
+        alt="Leve uma vida com estilo"
+        width={0}
+        height={0}
+        sizes="100vw"
+        className="h-auto w-full px-5"
+      />
+
+      <ProductList products={products} title="Mais vendidos" />
+
+      <Image
+        src="/banner-02.svg"
+        alt="Leve uma vida com estilo"
+        width={0}
+        height={0}
+        sizes="100vw"
+        className="h-auto w-full px-5"
+      />
     </div>
-  );
+  )
 }
